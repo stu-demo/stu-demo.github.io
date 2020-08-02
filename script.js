53151772e4f07c1e9c57acea0caeaa3fd6e614a8
Manukian <vaganmanuk@gmail.com>
2020-08-02 15:50:57 +0300

    header fixed

--- script.js
+++ script.js
@@ -4,22 +4,6 @@ window.onload = function () {
   var movie = document.getElementsByClassName('movie');
   var time = document.getElementsByClassName('time');
 
-  var title = document.getElementById('title');
-  var age = document.getElementById('age');
-  var movietime = document.getElementById('movie-time');
-  var year = document.getElementById('year');
-  var genre = document.getElementById('genre');
-  var country = document.getElementById('country');
-  var director = document.getElementById('director');
-  var language = document.getElementById('language');
-  var teaser = document.getElementById('teaser');
-  var description = document.getElementById('description');
-  var popup = document.getElementById('popup');
-
-  var info = [title,age,movietime,year,genre,country,director,language,description];
-
-  var close = document.getElementById('close-button');
-
 
   /*SLIDER*/
   show1.onclick = function() {
@@ -45,66 +29,123 @@ window.onload = function () {
   }
 
 
-  /*POSTER*/
-  var hover = function(e) {
-    document.getElementById('movie-data-wrapper').style.display = "block";
-    el = e.target.children[0].nextElementSibling.children[0];
-    document.getElementById('movie-data-title').textContent = el.textContent;
-    document.getElementById('movie-data-age').textContent = el.nextElementSibling.textContent;
-    document.getElementById('movie-data-time').textContent = el.nextElementSibling.nextElementSibling.textContent;
-  }
-
-  var mouseleave = function() {
-    document.getElementById('movie-data-wrapper').style.display = "none";
-  }
-
-
-   /*POPUP*/
-  var click = function(e) {
-    popup.style.display = "block";
-    popup.style.height = window.innerHeight + 'px';
-    document.body.style.overflowY = 'hidden';
-    el = e.target.nextElementSibling.childNodes[0].nextElementSibling;
-    var i = 0;
-    while (el) {
-      info[i].textContent = el.textContent;
-      console.log(info[i].textContent);
-      el = el.nextElementSibling;
-      i++;
-    }
-  }
-
-  close.onclick = function() {
-    popup.style.display = "none";
-    document.body.style.overflowY = 'auto';
-  }
-
-  for (var i=0; i<movie.length; i++) {
-    movie[i].addEventListener('mouseenter',hover);
-    movie[i].addEventListener('mouseleave',mouseleave);
-    movie[i].addEventListener('click',click);
-  }
-
-
 
   /*MENU ANIMATION*/
-  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   var logo = document.getElementById('logo');
   var date = document.getElementsByClassName('date');
   var menulinks = document.getElementsByClassName('menu-link');
   var padd = 12;
-  if (isMobile) {
-    padd = 9
+  if (window.innerWidth < 768) {
+    padd = 9;
   } else {
     document.body.onscroll = function() {
       var position = window.innerHeight-logo.offsetHeight-3*padd-date[0].clientHeight;
-      console.log(position);
       var zerotoone = window.scrollY/(document.body.scrollHeight-window.innerHeight);
-      console.log(zerotoone);
       for (i=0; i<menulinks.length; i++) {
         menulinks[i].style.transform = 'translateY(' + (position + padd*(i-4))*zerotoone/4*(i-4) + 'px)';
       }
     }
   }
+
+
+
+  /*POPUP*/
+  var popup = document.getElementById('popup');
+  var title = document.getElementById('title');
+  var age = document.getElementById('age');
+  var movietime = document.getElementById('movie-time');
+  var year = document.getElementById('year');
+  var genre = document.getElementById('genre');
+  var country = document.getElementById('country');
+  var director = document.getElementById('director');
+  var language = document.getElementById('language');
+  var teaser = document.getElementById('teaser');
+  var description = document.getElementById('description');
+  var info = [title,age,movietime,year,genre,country,director,language,description];
+  var close = document.getElementById('close-button');
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  var showpopup = function(e) {
+  e.stopPropagation();
+  popup.style.display = "block";
+  if (window.innerWidth < 768) {
+    popup.style.height = window.innerHeight + 'px';
+    document.body.style.overflowY = 'hidden';
+  }
+  for (var i=0; i < date.length; i++) {
+    date[i].style.opacity = '0';
+  }
+  var el = e.target;
+  //var el = e.currentTarget.children[1].children[0];
+  console.log(el);
+  if (el == '[object HTMLDivElement]') {
+    el = el.children[1].children[0];
+  } else {
+    el = el.nextElementSibling.children[0];
+  }
+  var i = 0;
+  while (el) {
+    info[i].textContent = el.textContent;
+    console.log(info[i].textContent);
+    el = el.nextElementSibling;
+    i++;
+    }
+  }
+  
+  close.onclick = function() {
+    popup.style.display = "none";
+    if (window.innerWidth < 768) {
+      document.body.style.overflowY = 'auto';
+    }
+    for (var i=0; i < date.length; i++) {
+      date[i].style.opacity = '1';
+    }
+  }
+
+  /*POSTER*/
+  if (window.innerWidth >= 768) {
+    var hover = function(e) {
+      document.getElementById('movie-data-wrapper').style.display = "block";
+      var element = e.target.children[0].nextElementSibling.children[0];
+      document.getElementById('movie-data-title').textContent = element.textContent;
+      document.getElementById('movie-data-age').textContent = element.nextElementSibling.textContent;
+      document.getElementById('movie-data-time').textContent = element.nextElementSibling.nextElementSibling.textContent;
+    }
+
+    var mouseleave = function() {
+      document.getElementById('movie-data-wrapper').style.display = "none";
+    }
+
+    for (var i=0; i<movie.length; i++) {
+      movie[i].addEventListener('mouseenter',hover);
+      movie[i].addEventListener('mouseleave',mouseleave);
+      movie[i].addEventListener('click',showpopup);
+    }
+  } else {
+    var movietouchstart = function(e) {
+      e.stopPropagation();
+      var touch = e.targetTouches[0];
+      if (touch.target == '[object HTMLDivElement]') {
+        var element = touch.target.children[1].children[0];
+      } else {
+        var element = touch.target.nextElementSibling.children[0];
+      }
+      document.getElementById('movie-data-wrapper').style.display = "block";
+      document.getElementById('movie-data-title').textContent = element.textContent;
+      document.getElementById('movie-data-age').textContent = element.nextElementSibling.textContent;
+      document.getElementById('movie-data-time').textContent = element.nextElementSibling.nextElementSibling.textContent;
+    }
+  
+    var movietouchend = function(e) {
+      document.getElementById('movie-data-wrapper').style.display = "none";
+    }
+  
+    for (var i=0; i<movie.length; i++) {
+      movie[i].addEventListener('touchstart',movietouchstart);
+      movie[i].addEventListener('touchend',movietouchend);
+      movie[i].addEventListener('click',showpopup);
+    }
+  }
+
 
 };
