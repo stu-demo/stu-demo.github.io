3560fbd997c0e0dc7ab04bb94ee6c3d8c7d94deb
Manukian <vaganmanuk@gmail.com>
2020-08-14 14:41:09 +0300

    sticky pic

--- menu.js
+++ menu.js
@@ -12,14 +12,14 @@ window.onload = function () {
     /*MENU ANIMATION*/
     var logo = document.getElementById('logo');
     var menulinks = document.getElementsByClassName('menu-link');
-    var d = menulinks[0].clientHeight+7;
+    var d = menulinks[0].clientHeight+14;
     console.log(d);
     if (window.innerWidth >= 768) {
       document.body.onscroll = function() {
         var position = window.innerHeight-logo.offsetHeight;
         var zerotoone = window.scrollY/(document.body.scrollHeight-window.innerHeight);
         for (i=0; i<menulinks.length; i++) {
-          menulinks[i].style.transform = 'translateY(' + (position + d*(i-4))*zerotoone/4*(i-4) + 'px)';
+          menulinks[i].style.transform = 'translateY(' + (position + d*(i-2))*zerotoone/2*(i-2) + 'px)';
         }
       }
     }
